80b445b480f79f5440d81e46d736f58b256e433c
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-08 21:48:39 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fungsi_bawaan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int x, y;
+        x = 10;
+        y = 15;
+        int sum = Add(x,y);
+            Console.WriteLine($"Sum is {sum}");
+            Console.ReadKey();
+
+        }
+static int Add(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+    }
+}
